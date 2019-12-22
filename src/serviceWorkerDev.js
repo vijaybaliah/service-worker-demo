715faafce5823625e9fde637092b6e828059c87c
vijay <vbalu@falabella.com>
2019-12-22 14:59:45 +0530

    feat: initialised service worker

---
+++ src/serviceWorkerDev.js
@@ -0,0 +1,10 @@
+export default registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/sw.js`).then(register => {
+      console.log('Worked: ', register)
+    })
+    .catch(error => {
+      console.log('Error: ', error)
+    })
+  }
+}
